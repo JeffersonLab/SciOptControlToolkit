c45721a0813a12659b2ab0631c28e9711066301f
schram <schram@jlab.org>
2023-03-10 17:38:07 -0500

    Initial version of the MBRL agent

--- jlab_rl/agents/__init__.py
+++ jlab_rl/agents/__init__.py
@@ -5,6 +5,8 @@ from jlab_rl.agents.keras_td3 import KerasTD3
 # from jlab_rl.agents.keras_td3_critic_dgpa import KerasTD3CriticDGPA
 from jlab_rl.agents.keras_td3_action_dgpa_v1 import KerasTD3ActorDGPA as KerasTD3ActorDGPA_v1
 from jlab_rl.agents.keras_td3_action_dgpa_v2 import KerasTD3ActorDGPA as KerasTD3ActorDGPA_v2
+from jlab_rl.agents.keras_td3_critic_dgpa_v2 import KerasTD3CriticDGPA as KerasTD3CriticDGPA_v2
+from jlab_rl.agents.keras_modelbased_agent import KerasGenericModelBasedAgent
 
 # Register TD3 agent
 register(
@@ -12,11 +14,11 @@ register(
     entry_point='jlab_rl.agents:KerasTD3'
 )
 
-# # Register TD3 w/ DGPA Critic
-# register(
-#     id='KerasTD3CriticDGPA-v0',
-#     entry_point='jlab_rl.agents:KerasTD3CriticDGPA'
-# )
+# Register TD3 w/ DGPA Actor using Steven model
+register(
+    id='KerasTD3ActorDGPA-v2',
+    entry_point='jlab_rl.agents:KerasTD3ActorDGPA_v2'
+)
 
 # Register TD3 w/ DGPA Actor using the original model
 register(
@@ -24,8 +26,13 @@ register(
     entry_point='jlab_rl.agents:KerasTD3ActorDGPA_v1'
 )
 
-# Register TD3 w/ DGPA Actor using Steven model
+# Register TD3 w/ DGPA Critic using Steven model
 register(
-    id='KerasTD3ActorDGPA-v2',
-    entry_point='jlab_rl.agents:KerasTD3ActorDGPA_v2'
+    id='KerasTD3CriticDGPA-v2',
+    entry_point='jlab_rl.agents:KerasTD3CriticDGPA_v2'
+)
+
+register(
+    id='KerasGenericModelBaseAdgent-v0',
+    entry_point='jlab_rl.agents:KerasGenericModelBasedAgent'
 )
