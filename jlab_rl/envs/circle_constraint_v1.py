9250b6349397543a103653ecbc3f06933f071d5e
schram <schram@jlab.org>
2023-07-05 09:30:44 -0400

    New files for the generative RL code

---
+++ jlab_rl/envs/circle_constraint_v1.py
@@ -0,0 +1,46 @@
+import gym
+from gym import spaces
+from jlab_rl.utils.circle_rdm import circle_rdm_samples
+
+import numpy as np
+
+class circle_constraint_env(gym.Env):
+    def __init__(self):
+        self.ndim = 2
+        self.action_space = spaces.Box(low=-np.ones(self.ndim), high=np.ones(self.ndim), dtype=np.float64)
+        self.observation_space = spaces.Box(low=-np.ones(self.ndim), high=np.ones(self.ndim), dtype=np.float64)
+        self.states, _ = self.reset()
+        self.delta_r = 0.25
+        self.delta_r_max = 1.0
+        self.target_radius = 0.95
+
+    def step(self, action):
+        print('Pre-states:', self.states)
+        print('Actions:', action)
+        self.states = self.states + action
+        print('Post-states:', self.states)
+        radius = np.sqrt(self.states[0]*self.states[0]+self.states[1]*self.states[1])
+        print('Post-radius:', radius)
+        radius_sqrt = np.square(radius - self.target_radius)
+        reward = - np.log(np.abs(radius - self.target_radius)) - 100 * np.square(radius - self.target_radius)
+        #reward = - radius_sqrt# -np.log(np.abs(radius-self.target_radius)) # Log-Linear reward
+        if self.states.any() > 1:
+            reward = -99
+        if self.states.any() < -1:
+            reward = -99
+        # if radius > 1:
+        #     reward = -99
+
+        print('reward:', reward)
+
+        # radius_sqrt = np.square(radius - self.target_radius)
+        # reward = np.exp(-100 * np.square(test_state - ideal_r))
+        # radius_sqrt = np.square(radius - self.target_radius)
+
+        return self.states, reward, False, False, {}
+
+    def reset(self):
+        self.states, _, _ = circle_rdm_samples(self.ndim, 1, 1.0, 0.75, give_all=True)
+        self.states = np.abs(self.states)
+        return self.states, ''
+
---
+++ jlab_rl/models/constraint_circle_state_generator.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+
+class ConstraintCircleGenerator(tf.keras.Model):
+
+  def __init__(self, ndims=2, nlayers=5, lower_bound=None, upper_bound=None):
+    super().__init__()
+    self.ndims = ndims
+    self.nlayers = nlayers
+    init = tf.keras.initializers.GlorotUniform()
+    nmul = 4
+    self.denses1 = []
+    self.bn1 = []
+    self.act1 = []
+    #self.mc_drops1 = []
+    for i in range(self.nlayers):
+      self.denses1.append(tf.keras.layers.Dense(nmul*32, kernel_initializer=init))
+      self.bn1.append(tf.keras.layers.BatchNormalization())
+      self.act1.append(tf.keras.layers.LeakyReLU(0.2))
+      #self.mc_drops1.append(tf.keras.layers.Dropout(0.15))
+
+    self.denses2 = []
+    self.bn2 = []
+    self.act2 = []
+    #self.mc_drops2 = []
+    for i in range(self.nlayers):
+      self.denses2.append(tf.keras.layers.Dense(nmul*32, kernel_initializer=init))
+      self.bn2.append(tf.keras.layers.BatchNormalization())
+      self.act2.append(tf.keras.layers.LeakyReLU(0.2))
+      #self.mc_drops2.append(tf.keras.layers.Dropout(0.15))
+
+    self.out1 = tf.keras.layers.Dense(self.ndims, kernel_initializer=init,
+                                      activation=tf.nn.leaky_relu)
+    self.out2 = tf.keras.layers.Dense(self.ndims, kernel_initializer=init,
+                                      activation=tf.nn.leaky_relu)
+    self.out = tf.keras.layers.Dense(self.ndims, kernel_initializer=init,
+                                     activation='tanh')
+    self.upper_bound = upper_bound
+    self.lower_bound = lower_bound
+
+  def call(self, inputs):
+    states, rdm_gaussians = inputs
+
+    # Find random samples in circle phase space
+    normalization = tf.math.sqrt(tf.math.reduce_sum(tf.math.square(rdm_gaussians), axis=1))
+    repeat_normalization = tf.repeat(tf.expand_dims(normalization, axis=1), self.ndims+2, axis=1)
+    rdm_circle_all = tf.math.divide(rdm_gaussians[:], repeat_normalization)
+    rdm_circle = rdm_circle_all[:,:-2]
+
+    # Random mask
+    x1 = rdm_circle_all
+    for i in range(0, self.nlayers):
+      x1 = self.denses1[i](x1)
+      x1 = self.bn1[i](x1)
+      x1 = self.act1[i](x1)
+      #x1 = self.mc_drops1[i](x1)
+    x1 = self.out1(x1)
+
+    # Add the rdm circle variables and state plots the generative prediction
+    x2 = tf.keras.layers.concatenate([states, rdm_circle])
+    for i in range(0, self.nlayers):
+      x2 = self.denses2[i](x2)
+      x2 = self.bn2[i](x2)
+      x2 = self.act2[i](x2)
+      #x2 = self.mc_drops2[i](x2)
+    x2 = self.out2(x2)
+
+    x = tf.keras.layers.concatenate([x1, x2])
+    x = self.out(x)
+
+    # Rescale using tanh [-1,1] to ensure it's within the parameter space
+    # if self.lower_bound.all() != None:
+    #   x = tf.keras.layers.Lambda(
+    #     lambda xi: ((xi + 1.0) * (self.upper_bound - self.lower_bound)) / 2.0 + self.lower_bound)(x)
+    return x
