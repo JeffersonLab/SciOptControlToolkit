d3c9c016ce83ffdf5f2cf0779e031afa5aa55d24
schram <schram@jlab.org>
2023-08-10 12:36:28 -0400

    Update ensemble code to include rdm action sampling until the buffer has at least the batch size data

--- jlab_rl/agents/keras_ensemble_grl.py
+++ jlab_rl/agents/keras_ensemble_grl.py
@@ -140,6 +140,11 @@ class KerasEnsembleGenerativeTD3(KerasTD3):
 
         self.nactions.assign(self.nactions + 1)
 
+        if self.buffer_counter < self.batch_size:
+            sampled_action = self.env.action_space.sample()
+            noise = np.zeros(self.num_actions)
+            return sampled_action, noise
+
         # Single try
         state = np.expand_dims(state, 0)
 
