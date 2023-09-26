7df6f26fd16184ade60f7cf6bce608bef5468b7c
Kishanrajput <kishansingh0817@gmail.com>
2023-09-26 17:16:45 -0400

    fix 2d distance score

--- jlab_rl/utils/score.py
+++ jlab_rl/utils/score.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 @tf.function
 def get_score(pred:tf.Tensor, obs:tf.Tensor) -> tf.Tensor:
     # Difference between top actions
-    score2 = tf.reduce_mean(tf.math.sqrt(tf.reduce_sum(tf.math.squared_difference(
-        tf.expand_dims(obs, axis=1), tf.expand_dims(obs, axis=0)), axis=-1)))
+    score2 = tf.reduce_sum(tf.math.sqrt(tf.reduce_sum(tf.math.squared_difference(
+        tf.expand_dims(obs, axis=1), tf.expand_dims(obs, axis=0)), axis=-1))) / (obs.shape[0]*(obs.shape[0]-1))
 
     # Difference between current and top action
     score1 = tf.reduce_mean(tf.math.sqrt(tf.reduce_sum(tf.math.squared_difference(
