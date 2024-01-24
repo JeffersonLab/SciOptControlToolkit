bf68bc98aaa3d65aa83a0a1f1495beb94bccb52c
Kishansingh Rajput <kishansingh0817@gmail.com>
2024-01-24 10:53:54 -0500

    TD3 to support gymnasium dict space for obs

--- jlab_opt_control/agents/keras_td3.py
+++ jlab_opt_control/agents/keras_td3.py
@@ -37,6 +37,7 @@ import json
 import platform
 import sys
 processor = platform.processor()
+import gymnasium
 
 import logging
 
@@ -64,7 +65,15 @@ class KerasTD3(jlab_opt_control.Agent):
         self.env = env
         try:
             assert "Box" in str(type(env.action_space)), 'Invalid action space'
-            self.num_states = env.observation_space.shape[0]
+            ##################################################################
+            print("Obs type: ", type(env.observation_space))
+            if isinstance(env.observation_space, gymnasium.spaces.Dict):
+                self.num_states = 0
+                for key in list(env.observation_space.keys()):
+                    self.num_states += env.observation_space[key].shape[0]
+            else:
+                self.num_states = env.observation_space.shape[0]
+            ##################################################################
             self.num_actions = env.action_space.shape[0]
             self.upper_bound = env.action_space.high
             self.lower_bound = env.action_space.low
