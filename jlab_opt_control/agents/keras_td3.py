1405b492b541e245dae72549a512175807508cab
armenkasp <armenk@jlab.org>
2024-02-06 11:24:21 -0500

    Adding in model makes

--- jlab_opt_control/agents/keras_td3.py
+++ jlab_opt_control/agents/keras_td3.py
@@ -28,6 +28,7 @@
 
 import jlab_opt_control as jlab_opt_control
 import jlab_opt_control.utils.cfg_utils as cfg_utils
+import jlab_opt_control.models
 import tensorflow as tf
 from tensorflow.keras import layers
 import numpy as np
@@ -44,44 +45,6 @@ import logging
 td3_log = logging.getLogger("TD3-Agent")
 td3_log.setLevel(logging.DEBUG)
 logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
-
-
-class Actor(tf.keras.Model):
-    def __init__(self, state_dim, action_dim, min_action, max_action):
-        super().__init__()
-
-        # Actor Architecture
-        self.l1 = layers.Dense(256, activation="relu", input_shape=(state_dim,))
-        self.l2 = layers.Dense(256, activation="relu")
-        self.l3 = layers.Dense(action_dim, activation='tanh')
-
-        self.action_scale = tf.constant((max_action - min_action) / 2, dtype=tf.float32)
-        self.action_bias = tf.constant((max_action + min_action) / 2, dtype=tf.float32)
-
-        self.max_action = max_action
-
-    def call(self, state, training=False):
-        a = self.l1(state)
-        a = self.l2(a)
-        a = self.l3(a)
-        return a * self.action_scale + self.action_bias
-
-class Critic(tf.keras.Model):
-    def __init__(self, state_dim, action_dim):
-        super().__init__()
-
-        # Q network Architecture
-        self.l1 = layers.Dense(256, activation="relu", input_shape=(state_dim + action_dim,))
-        self.l2 = layers.Dense(256, activation="relu")
-        self.l3 = layers.Dense(1)
-
-    def call(self, state, action, training=False):
-        x = tf.concat([state, action], axis=1)
-        x = self.l1(x)
-        x = self.l2(x)
-        x = self.l3(x)
-
-        return x
 
 class KerasTD3(jlab_opt_control.Agent):
 
@@ -129,6 +92,9 @@ class KerasTD3(jlab_opt_control.Agent):
 
         self.model_load_path = cfg_utils.cfg_get(data, 'load_model', None)
         self.model_save_path = cfg_utils.cfg_get(data, 'save_model', None)
+
+        self.actor_model_type = cfg_utils.cfg_get(data, 'actor_model', "actor_FCNN-v0")
+        self.critic_model_type = cfg_utils.cfg_get(data, 'critic_model', "critic_FCNN-v0")
 
         self.logdir = logdir
 
@@ -187,8 +153,14 @@ class KerasTD3(jlab_opt_control.Agent):
         """ Initialize new models from scratch """
         td3_log.info('Running KerasTD3 initialize_new_models()')
 
-        self.actor_model = Actor(self.num_states, self.num_actions, self.lower_bound, self.upper_bound)
-        self.target_actor = Actor(self.num_states, self.num_actions, self.lower_bound, self.upper_bound)
+        # self.actor_model_type
+        # self.critic_model_type
+
+        self.actor_model = jlab_opt_control.models.make(self.actor_model_type, self.num_states, self.num_actions, self.lower_bound, self.upper_bound)
+        self.target_actor = jlab_opt_control.models.make(self.actor_model_type, self.num_states, self.num_actions, self.lower_bound, self.upper_bound)
+
+        # self.actor_model = Actor(self.num_states, self.num_actions, self.lower_bound, self.upper_bound)
+        # self.target_actor = Actor(self.num_states, self.num_actions, self.lower_bound, self.upper_bound)
         
         seed1 = time.time_ns()
         str_seed1 = str(seed1)
