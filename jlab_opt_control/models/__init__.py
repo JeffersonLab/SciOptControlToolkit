5057fa4b58051f89b9c5680d508d730c14b0cebc
armenkasp <armenk@jlab.org>
2024-02-29 14:41:21 -0500

    added models to corresponding init file

--- jlab_opt_control/models/__init__.py
+++ jlab_opt_control/models/__init__.py
@@ -28,8 +28,10 @@
 
 from jlab_opt_control.models.registration import register, make, list_registered_modules
 from jlab_opt_control.models.actor_fcnn import ActorFCNN
+from jlab_opt_control.models.mo_actor_fcnn import MO_ActorFCNN
 from jlab_opt_control.models.actor_fcnn_v2 import ActorFCNN_v2
 from jlab_opt_control.models.critic_fcnn import CriticFCNN
+from jlab_opt_control.models.mo_critic_fcnn import MO_CriticFCNN
 
 register(
     id='actor_fcnn-v0',
