6997bca658cea37bf175a1874306af8f6548eaa4
schram <schram@jlab.org>
2024-04-25 13:00:37 -0400

    Generalized SINDy code to allow hard output boundary constraints.

--- jlab_opt_control/models/uqsindy_network.py
+++ jlab_opt_control/models/uqsindy_network.py
@@ -18,7 +18,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
 
 class UQSINDyNetwork(Model):
     #def __init__(self, logdir, cfg='uqsindy_network.cfg'):
-    def __init__(self, num_features_in, num_features_out, batch_size, logdir, cfg='uqsindy_network.cfg'):
+    def __init__(self, num_features_in, num_features_out, min_action=None, max_action=None, batch_size=128, logdir='./results', cfg='uqsindy_network.cfg'):
         super().__init__()
         
         # Load configuration
@@ -36,6 +36,12 @@ class UQSINDyNetwork(Model):
         # num_features_in = cfg_data.get("num_features_in", 10) #Default
         # num_features_out = cfg_data.get("num_features_out", 1) #Default
         # self.batch_size = cfg_data.get("batch_size", 1024)
+
+        self.using_tanh = False
+        if max_action!=None and min_action!=None:
+            self.action_scale = tf.constant((max_action - min_action) / 2, dtype=tf.float32)
+            self.action_bias = tf.constant((max_action + min_action) / 2, dtype=tf.float32)
+            self.using_tanh = True
 
         hidden_layers = cfg_data.get('hidden_layers', 2)  # Default to 2 if not specified
         nodes_per_layer = cfg_data.get('nodes_per_layer', [256, 256])  # Default
@@ -70,9 +76,10 @@ class UQSINDyNetwork(Model):
         # Output layer with its specified activation function
         self.output_layer = layers.Dense(total_features, activation=activation_functions[-1])
 
-    def sample_posterior(self, batch_size=1024):
+    @tf.function
+    def sample_posterior(self, sampling_size=1024):
         # Draw from latent distribution
-        randn = tf.random.normal(shape=(batch_size, *self.mu.shape), dtype=tf.float32)
+        randn = tf.random.normal(shape=(sampling_size, *self.mu.shape), dtype=tf.float32)
         #print(f'randn: {randn.shape}')
         betas = self.mu[None] + randn * tf.exp(0.5 * self.log_var[None])
         #print(f'betas 1: {betas.shape}')
@@ -93,6 +100,7 @@ class UQSINDyNetwork(Model):
         #print(f'betas 4: {betas.shape}')
         return betas
 
+    @tf.function
     def call(self, inputs, nsamples=0, training=False):
         """ forward pass of model """
         if nsamples==0:
@@ -100,6 +108,15 @@ class UQSINDyNetwork(Model):
         x = inputs
         betas = self.sample_posterior(nsamples)
         BX = tf.einsum('nd,bdo->bno', x, betas)
+        #print(f'BX: {BX.shape}')
+        if self.using_tanh:
+            BX = tf.keras.activations.tanh( BX[0,:,:] )
+            BX = BX  * self.action_scale + self.action_bias
+        #print(f'BX: {BX.shape}')
+
+        # upper_bound = 2
+        # lower_bound = -2
+        # BX = tf.keras.layers.Lambda(lambda x: ((x + 1.0) * (upper_bound - lower_bound)) / 2.0 + self.lower_bound)(BX)
         return BX
 
     # def negative_log_likelihood(self, x, y0):
