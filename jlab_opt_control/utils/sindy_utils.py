7531db733da5805966ee1402a4e8571df3304c89
schram <schram@jlab.org>
2024-04-26 10:43:44 -0400

    Reformated using black PIP8 formating

--- jlab_opt_control/utils/sindy_utils.py
+++ jlab_opt_control/utils/sindy_utils.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 
+
 class FourierLibrary(tf.keras.layers.Layer):
-    '''
+    """
     Library of fourier functions up to a specified number of frequencies
     Generally following the scikit-learn Transformer style
-    '''
+    """
+
     def __init__(self, include_sin=True, include_cos=True, n_frequencies=1):
         super().__init__()
 
@@ -25,45 +27,45 @@ class FourierLibrary(tf.keras.layers.Layer):
         else:
             self.output_dim_ = self.n_frequencies * x.shape[-1]
         return self
-        
+
     def transform(self, x):
         return self.call(x)
-    
+
     def fit_transform(self, x):
         self.fit(x)
         return self.transform(x)
 
     def get_feature_names(self, input_features=None):
         if input_features is None:
-            input_features = [f'x{i}' for i in range(self.input_dim_)]
+            input_features = [f"x{i}" for i in range(self.input_dim_)]
         assert len(input_features) == self.input_dim_
 
         freqs = []
         for feat in input_features:
             for i in range(1, self.n_frequencies + 1):
-                freqs.append(f'{i} {feat}')
+                freqs.append(f"{i} {feat}")
 
         feature_names = []
         if self.include_sin:
-            feature_names = feature_names + [f'sin {freq}' for freq in freqs]
+            feature_names = feature_names + [f"sin {freq}" for freq in freqs]
         if self.include_cos:
-            feature_names = feature_names + [f'cos {freq}' for freq in freqs] 
-        
+            feature_names = feature_names + [f"cos {freq}" for freq in freqs]
+
         return feature_names
-    
+
     def call(self, x):
         n_samples, n_features = x.shape
         assert n_features == self.input_dim_
 
-        freqs = tf.range(1, self.n_frequencies+1, dtype=x.dtype)
-        xf = tf.einsum('bl,f->blf', x, freqs)
-        xf = tf.reshape(xf, [n_samples, self.n_frequencies*n_features])
+        freqs = tf.range(1, self.n_frequencies + 1, dtype=x.dtype)
+        xf = tf.einsum("bl,f->blf", x, freqs)
+        xf = tf.reshape(xf, [n_samples, self.n_frequencies * n_features])
 
         library = []
         if self.include_sin:
             library.append(tf.sin(xf))
         if self.include_cos:
             library.append(tf.cos(xf))
-        
-        library = tf.concat(library, axis=1) #[n_samples, n_features]
+
+        library = tf.concat(library, axis=1)  # [n_samples, n_features]
         return library
