6cadb74e97a5bfc8b9d8d59cdbbf3fe442a78b29
schram <schram@jlab.org>
2024-04-26 10:59:24 -0400

    Remove files unrelated to the issue.

--- jlab_opt_control/agents/__init__.py
+++ jlab_opt_control/agents/__init__.py
@@ -29,7 +29,6 @@
 from jlab_opt_control.agents.registration import register, make, list_registered_modules
 from jlab_opt_control.agents.keras_td3 import KerasTD3
 from jlab_opt_control.agents.keras_ddpg import KerasDDPG
-from jlab_opt_control.agents.genai_sindy_td3 import GenAISINDyTD3
 
 # Single Objective Agents
 register(
@@ -42,9 +41,4 @@ register(
     id='KerasDDPG-v0',
     entry_point='jlab_opt_control.agents:KerasDDPG',
     kwargs={'cfg': 'keras_ddpg.cfg'},
-)
-register(
-    id='GenAI-SINDyTD3-v0',
-    entry_point='jlab_opt_control.agents:GenAISINDyTD3',
-    kwargs={'cfg': 'genai_keras_td3.cfg'},
 )
