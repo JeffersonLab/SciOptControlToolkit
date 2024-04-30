8e6fa502b73fa1920903c5f9f5b528dbc7ab836f
Jonathan Colen <jcolen19@gmail.com>
2024-04-30 15:54:12 -0400

    Separated libraries into separate files in utils/sindy_lib folder

--- utests/utest_sindy.py
+++ utests/utest_sindy.py
@@ -3,20 +3,21 @@ import numpy as np
 import os
 
 import jlab_opt_control.models
-from jlab_opt_control.utils.sindy_lib_utils import PolynomialLibrary
+from jlab_opt_control.utils.sindy_lib.polynomial_library import PolynomialLibrary
 import tensorflow as tf
+
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
         rng = np.random.default_rng(1)
         # Create data following parameters from UQ-SINDy paper
-        self.X = rng.normal(loc=0., scale=1., size=[400, 10])
-        print('self.X:',self.X.shape)
-        eps = rng.normal(loc=0., scale=0.25, size=[400, 1])
+        self.X = rng.normal(loc=0.0, scale=1.0, size=[400, 10])
+        print("self.X:", self.X.shape)
+        eps = rng.normal(loc=0.0, scale=0.25, size=[400, 1])
         self.beta = np.array([[0.3, 0.2, -0.3, 0, 0, 0, 0, 0, 0, 0]]).T
-        print('self.beta:',self.beta.shape)
+        print("self.beta:", self.beta.shape)
         self.y0 = self.X @ self.beta + eps
-        print('self.y0:',self.y0.shape)
+        print("self.y0:", self.y0.shape)
 
     def test_numpy_lstsq(self):
         beta = np.linalg.lstsq(self.X, self.y0, rcond=None)[0]
@@ -24,7 +25,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_sindy_network(self):
         # Setup model
-        model = jlab_opt_control.models.make('sindy_network-v0', logdir='results/test')
+        model = jlab_opt_control.models.make("sindy_network-v0", logdir="results/test")
 
         # Setup optimizers
         optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2, epsilon=1e-8)
@@ -57,26 +58,28 @@ class MyTestCase(unittest.TestCase):
 
     def test_uqsindy_network(self):
         # Convert everything to tensors
-        print(f'self.X: {self.X.shape}')
+        print(f"self.X: {self.X.shape}")
         X_batch = tf.convert_to_tensor(self.X, dtype=tf.float32)
         y0_batch = tf.convert_to_tensor(self.y0, dtype=tf.float32)
-        print(f'X_batch: {X_batch.shape}')
-        print(f'y0_batch: {y0_batch.shape}')
+        print(f"X_batch: {X_batch.shape}")
+        print(f"y0_batch: {y0_batch.shape}")
 
         # Setup library
         num_poly = 4
         library = PolynomialLibrary(degree=num_poly, include_bias=False)
         library.fit(X_batch)
         lib_batch = library(X_batch)
-        print(f'lib_batch: {library.output_dim_}')
-        print(f'lib_batch: {lib_batch.shape}')
+        print(f"lib_batch: {library.output_dim_}")
+        print(f"lib_batch: {lib_batch.shape}")
 
         # Setup model
-        model = jlab_opt_control.models.make('uqsindy_network-v0',
-                                             num_features_in=library.output_dim_,
-                                             num_features_out=y0_batch.shape[1],
-                                             batch_size = self.X.shape[0],
-                                             logdir='results/test')
+        model = jlab_opt_control.models.make(
+            "uqsindy_network-v0",
+            num_features_in=library.output_dim_,
+            num_features_out=y0_batch.shape[1],
+            batch_size=self.X.shape[0],
+            logdir="results/test",
+        )
 
         # Setup optimizers
         optimizer = tf.keras.optimizers.Adam(learning_rate=1e-2, epsilon=1e-8)
@@ -90,14 +93,15 @@ class MyTestCase(unittest.TestCase):
             with tf.GradientTape() as tape:
                 neg_log_p_Xy = model.negative_log_likelihood(lib_batch, y0_batch)
                 loss = tf.reduce_mean(neg_log_p_Xy) + model.kld()
-            
+
             gradients = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        
-        beta = tf.reduce_mean(model.sample_posterior(sampling_size=10), axis=0).numpy()
-        print(f'self.beta: {self.beta.shape}')
-        print(f'beta: {beta.shape}')
-        #self.assertTrue(np.allclose(beta, self.beta, atol=5e-2))
 
-if __name__=='__main__':
+        beta = tf.reduce_mean(model.sample_posterior(sampling_size=10), axis=0).numpy()
+        print(f"self.beta: {self.beta.shape}")
+        print(f"beta: {beta.shape}")
+        # self.assertTrue(np.allclose(beta, self.beta, atol=5e-2))
+
+
+if __name__ == "__main__":
     unittest.main()
