79678f81f73b8b11eab1ab442e2b1702b12b3b3e
Armen Kasparian <armenk@jlab.org>
2024-06-07 16:33:15 -0400

    Hot fix adding the file name in init

--- jlab_opt_control/models/__init__.py
+++ jlab_opt_control/models/__init__.py
@@ -30,6 +30,7 @@ from jlab_opt_control.models.registration import register, make, list_registered
 from jlab_opt_control.models.actor_fcnn import ActorFCNN
 from jlab_opt_control.models.actor_fcnn_v2 import ActorFCNN_v2
 from jlab_opt_control.models.critic_fcnn import CriticFCNN
+from jlab_opt_control.models.critic_fcnn_v1 import CriticFCNN_v1
 from jlab_opt_control.models.critic_fcnn_v2 import CriticFCNN_v2
 from jlab_opt_control.models.sindy_network import SINDyNetwork
 from jlab_opt_control.models.uqsindy_network import UQSINDyNetwork
