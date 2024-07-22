18e370bda95ee1993ab891f092be2caefbd12e98
Armen Kasparian <armenk@jlab.org>
2024-07-22 16:39:53 -0400

    Added check to make sure the agent is training before running inference

--- jlab_opt_control/drivers/run_continuous.py
+++ jlab_opt_control/drivers/run_continuous.py
@@ -236,7 +236,7 @@ def run_opt(index, max_nepisodes, max_nsteps, agent_id, env_id, logdir, buffer_t
                             data=episodic_reward, step=int(ep))
 
             # Run inference test
-            if ep % 1 == 0:
+            if ep % 1 == 0 and agent.buffer.size() > np.max([agent.batch_size, agent.warmup_size]):
                 agent.buffer.save()
                 inference_episodic_reward = 0
                 inference_prev_state, _ = env.reset()
