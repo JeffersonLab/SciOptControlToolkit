cef80742182ed0d48e9a8a098be63b0fac5f3fe2
armenkasp <armenk@jlab.org>
2024-08-08 14:01:43 -0400

    Fixed logging in agents for tensorboard dependent on number of actions

--- jlab_opt_control/agents/keras_ddpg.py
+++ jlab_opt_control/agents/keras_ddpg.py
@@ -299,25 +299,18 @@ class KerasDDPG(jlab_opt_control.Agent):
             assert sampled_action.shape == self.num_actions or sampled_action.shape == (self.num_actions,), \
                 f"Sampled action shape is incorrect... {sampled_action.shape}"
 
-        # Log the training action(s) taken
+        # Log the training/inference action(s) taken
         if train:
-            self.nactions = self.nactions + 1
-            if self.num_actions == 0:
-                tf.summary.scalar('Action', data=sampled_action,
-                                  step=int(self.nactions))
-            else:
-                for i in range(self.num_actions):
+            self.nactions += 1
+            for i in range(self.num_actions):
                     tf.summary.scalar('Action #{}'.format(
                         i), data=sampled_action[i], step=int(self.nactions))
         if inference:
-            self.inf_nactions = self.inf_nactions + 1
-            if self.num_actions == 0:
-                tf.summary.scalar('Inference Action', data=sampled_action,
-                                  step=int(self.inf_nactions))
-            else:
-                for i in range(self.num_actions):
-                    tf.summary.scalar('Inference Action #{}'.format(
-                        i), data=sampled_action[i], step=int(self.inf_nactions))
+            self.inf_nactions += 1
+            for i in range(self.num_actions):
+                tf.summary.scalar('Inference Action #{}'.format(
+                    i), data=sampled_action[i], step=int(self.inf_nactions))
+                    
         # Insure action output by actor is in legal environment range
         return sampled_action, noise
 
