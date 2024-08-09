8bc97850656000e14e3b8e998c0ed6807b7adb75
Kishanrajput <kishansingh0817@gmail.com>
2024-08-08 21:07:31 -0400

    comment MO-KerasOpt - not available

--- jlab_opt_control/agents/__init__.py
+++ jlab_opt_control/agents/__init__.py
@@ -59,7 +59,7 @@ register(
 
 # Multi Objective Agents
 from jlab_opt_control.agents.mo_keras_td3 import MO_KerasTD3
-from jlab_opt_control.agents.mo_keras_opt import MO_KerasOpt
+# from jlab_opt_control.agents.mo_keras_opt import MO_KerasOpt
 from jlab_opt_control.agents.mo_keras_mb import MO_KerasMB
 
 register(
@@ -68,11 +68,11 @@ register(
     kwargs={'cfg': 'mo_keras_td3.cfg'},
 )
 
-register(
-    id='MO-KerasOpt-v0',
-    entry_point='jlab_opt_control.agents:MO_KerasOpt',
-    kwargs={'cfg': 'mo_keras_td3.cfg'},
-)
+# register(
+#     id='MO-KerasOpt-v0',
+#     entry_point='jlab_opt_control.agents:MO_KerasOpt',
+#     kwargs={'cfg': 'mo_keras_td3.cfg'},
+# )
 
 register(
     id='MO-KerasMB-v0',
