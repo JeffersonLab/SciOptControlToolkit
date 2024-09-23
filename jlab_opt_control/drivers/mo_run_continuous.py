55e1c74524cb4e8ac045a743b7dad39056b0e15f
schram <schram@jlab.org>
2024-09-23 18:16:05 -0400

    Updated a bunch things

--- jlab_opt_control/drivers/mo_run_continuous.py
+++ jlab_opt_control/drivers/mo_run_continuous.py
@@ -37,6 +37,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from matplotlib.backends.backend_agg import FigureCanvasAgg
+from pymoo.indicators.hv import Hypervolume
 from tqdm import tqdm
 
 import jlab_opt_control.agents
@@ -129,6 +130,41 @@ def run_opt(index, max_nepisodes, max_nsteps, agent_id, env_id, logdir, buffer_t
     agent.save_cfg()
     agent.save("init")
 
+    ga_results_loc = '../notebooks'
+    if ga_results_loc is not None:
+        if '8D' in env_id:
+            ga_results = np.load(os.path.join(ga_results_loc, "1L10_TEST8_nsga_II_results.npy"))
+            ref = [22.0, 0.04]
+            # ideal = [20.0, 0.01]
+            # metric = Hypervolume(ref_point= ref,
+            #              norm_ref_point=False,
+            #              zero_to_one=False,
+            #              ideal=ideal,
+            #              nadir=ref)
+            # max_vol = (ref[0] - ideal[0]) * (ref[1] - ideal[1])
+            # ga_hv = np.round(metric.do(ga_results)*100/max_vol, 3)
+            metric = Hypervolume(ref_point= ref)
+            ga_hv = metric.do(ga_results)
+        elif '-N-' in env_id:
+            ga_results = np.load(os.path.join(ga_results_loc, "NORTH_nsga_II_results.npy"))
+            #ga_results = np.load(os.path.join(ga_results_loc, "NORTH_nsga_II_results_045000.npy"))
+            #ref = [2530.0, 6.0]
+            ref = [3000.0, 20.0]
+            metric = Hypervolume(ref_point=ref)
+            # ideal = [2380.0, 1.0]
+            # metric = Hypervolume(ref_point= ref,
+            #              norm_ref_point=False,
+            #              zero_to_one=False,
+            #              ideal=ideal,
+            #              nadir=ref)
+            # max_vol = (ref[0] - ideal[0]) * (ref[1] - ideal[1])
+            # ga_hv = np.round(metric.do(ga_results)*100/max_vol, 3)
+            ga_hv = metric.do(ga_results)#*100/max_vol, 3)
+        else:
+            ga_results = None
+    else:
+        ga_results = None
+
     # To store reward history of each episode
     ep_reward_list = []
     # To store average reward history of last few episodes
@@ -145,9 +181,10 @@ def run_opt(index, max_nepisodes, max_nsteps, agent_id, env_id, logdir, buffer_t
     np_alphas = np.zeros((nscans,env.reward_space.shape[0]), dtype=np.float32)
     np_alphas[:, 1] = np.linspace(0.0,1.0, nscans) #array([1.00, 0.99, 0.95, 0.90, 0.85, 0.75, 0.50, 0])
     np_alphas[:, 0] = 1.0 - np_alphas[:,1]
-    print(f'np_alphas: {np_alphas}')
-    print(f'np_alphas: {np_alphas.shape}')
+    #print(f'np_alphas: {np_alphas}')
+    #print(f'np_alphas: {np_alphas.shape}')
 
+    nvalid_solutions = 0
     max_nepochs = 50000
     for epoch in tqdm(range(max_nepochs), desc='Epochs'):
         scans = np.random.rand(nscans)
@@ -170,6 +207,16 @@ def run_opt(index, max_nepisodes, max_nsteps, agent_id, env_id, logdir, buffer_t
                 state, reward, terminate, truncate, info = env.step(action)
                 run_openai_log.debug(f'reward: {reward}')
                 run_openai_log.debug(f'reward: {type(reward)}')
+                run_openai_log.debug(f'energy: {info["energy"]}')
+                run_openai_log.debug(f'terminate: {terminate}/{int(terminate)}')
+                if terminate==False:
+                    nvalid_solutions += 1
+                    #run_openai_log.info(f'info: {info}')
+
+                #sys.exit()
+                if "Pareto" in agent_id:
+                    reward = np.array([info['heat'], info['trip']])
+                    run_openai_log.debug(f'new reward: {reward}')
 
                 # Check shapes and data types
                 assert 'numpy.ndarray' in str(type(state))
@@ -184,6 +231,8 @@ def run_opt(index, max_nepisodes, max_nsteps, agent_id, env_id, logdir, buffer_t
 
         # Run inference test
         run_openai_log.info(f'total_nsteps {total_nsteps}')
+        if total_nsteps!=0:
+            run_openai_log.info(f'fraction of valid solutions: {float(nvalid_solutions/total_nsteps)}')
         if total_nsteps % 1000 == 0:
             run_openai_log.info(f'Running inference ...')
             inference_nscans = 250
@@ -209,15 +258,41 @@ def run_opt(index, max_nepisodes, max_nsteps, agent_id, env_id, logdir, buffer_t
                     inference_prev_state = inference_state
                     inference_done = (inference_terminate or inference_truncate)
 
-            print(f'Number of valid scans: {len(scan_trips)}')
-            if len(scan_trips)>0:
+            rl_points = np.stack([scan_heats, scan_trips], axis=1)
+            good_indices = np.where((rl_points[:, 0] <= ref[0]) & (rl_points[:, 1] <= ref[1]))[0]
+            print(f'Number of valid scans: {len(good_indices)}')
+            #print(f'Number of valid scans: {(good_indices)}')
+            scan_alphas = np.array(scan_alphas)
+            if len(good_indices)>0:
+                if ga_results is not None:
+                    ga_index = np.argsort(ga_results[:,0])
+                    ga_heat = ga_results[:,0]
+                    ga_trip = ga_results[:,1]
+                    plt.plot(ga_heat[ga_index], ga_trip[ga_index], c='black', linestyle='dashed', label=f'NSGA II (HV: {ga_hv:.3f})')
+
+                ##
+                rl_points = rl_points[good_indices]
+                trimmed_alphas = scan_alphas[good_indices]
+                print(rl_points.shape)
+                print(trimmed_alphas.shape)
+                # rl_hv = np.round(metric.do(rl_points)*100/max_vol, 3)
+                rl_hv = metric.do(rl_points)
+
                 fig, ax = plt.subplots(dpi=90)
                 # run_openai_log.info(f'scan_heats: {scan_heats}')
                 # run_openai_log.info(f'scan_trips: {scan_trips}')
                 # run_openai_log.info(f'scan_alphas: {scan_alphas}')
-                plt.scatter(scan_heats,scan_trips, s=100, c=scan_alphas)
+                #plt.scatter(scan_heats,scan_trips, s=100, c=scan_alphas)
+                plt.scatter(rl_points[:, 0],rl_points[:, 1], s=50, c=trimmed_alphas, label=f'MOTD3 (HV: {rl_hv:.3f})')
+                #np.sum(scan_rewards,axis=1))#scan_alphas)
+                if ga_results is not None:
+                    ga_index = np.argsort(ga_results[:,0])
+                    ga_heat = ga_results[:,0]
+                    ga_trip = ga_results[:,1]
+                    plt.plot(ga_heat[ga_index], ga_trip[ga_index], c='black', linestyle='dashed', label=f'NSGA II (HV: {ga_hv:.3f})')
+
                 # plt.xlim(20.8, 22.4)
-                plt.ylim(0.015, 0.05)
+                #plt.ylim(0.015, 0.05)
                 # Change major ticks to show every 20.
                 # ax.xaxis.set_major_locator(MultipleLocator(0.2))
                 # ax.yaxis.set_major_locator(MultipleLocator(0.005))
@@ -228,6 +303,7 @@ def run_opt(index, max_nepisodes, max_nsteps, agent_id, env_id, logdir, buffer_t
                 plt.ylabel('Trip Rate [per hour]');
                 plt.colorbar()
                 plt.tight_layout()
+                plt.legend()
                 plt.savefig(logdir+f'/pareto_steps{total_nsteps}_{inference_total_reward:.4f}.jpeg')
                 # Convert figure to an image tensor and log
                 buf = io.BytesIO()
