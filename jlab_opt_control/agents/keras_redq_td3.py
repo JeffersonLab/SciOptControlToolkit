098d0a9a8e3430bdebdee6911c72c1a0e928a5b6
armenkasp <armenk@jlab.org>
2025-03-11 08:55:57 -0400

    Initial red-q TD3 implementation

---
+++ jlab_opt_control/agents/keras_redq_td3.py
@@ -0,0 +1,413 @@
+import logging
+import jlab_opt_control as jlab_opt_control
+import jlab_opt_control.utils.cfg_utils as cfg_utils
+import jlab_opt_control.buffers
+import jlab_opt_control.models
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+import os
+from os.path import join
+import time
+import json
+import platform
+import sys
+import shutil
+import random
+processor = platform.processor()
+
+redq_log = logging.getLogger("REDQ-TD3-Agent")
+redq_log.setLevel(logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+
+
+class KerasREDQTD3(jlab_opt_control.Agent):
+
+    def __init__(self, env, logdir, buffer_type=None, buffer_size=None, cfg='keras_redq_td3.json'):
+        """ Define all key variables required for all agent """
+
+        # Get env info
+        self.critic_models = []
+        self.target_critics = []
+        self.target_actor = None
+        self.actor_model = None
+        redq_log.info('Running KerasREDQTD3 __init__')
+
+        # Environment setup
+        self.env = env
+        try:
+            assert "Box" in str(type(env.action_space)), 'Invalid action space'
+            self.num_states = env.observation_space.shape[0]
+            self.num_actions = env.action_space.shape[0]
+            self.upper_bound = env.action_space.high
+            self.lower_bound = env.action_space.low
+            redq_log.info(f'Action upper bound: {self.upper_bound}')
+            redq_log.info(
+                f'Action upper bound: {float(env.action_space.high[0])}')
+            redq_log.info(f'Action lower bound: {self.lower_bound}')
+            redq_log.info(
+                f'Action lower bound: {float(env.action_space.low[0])}')
+            self.range = self.upper_bound - self.lower_bound
+            redq_log.info(f'Action range: {self.range}')
+        except:
+            redq_log.error('Action space not valid for this agent.')
+            sys.exit(0)
+
+        # Load configuration
+        absolute_path = os.path.dirname(__file__)
+        relative_path = "../cfgs/"
+        full_path = os.path.join(absolute_path, relative_path)
+        self.pfn_json_file = os.path.join(full_path, cfg)
+        redq_log.debug(f'pfn_json_file:{self.pfn_json_file}')
+        with open(self.pfn_json_file) as json_file:
+            data = json.load(json_file)
+        self.warmup_size = int(cfg_utils.cfg_get(data, 'warmup_size', 10000))
+        self.batch_size = int(cfg_utils.cfg_get(data, 'batch_size', 100))
+        self.model_load_path = cfg_utils.cfg_get(data, 'load_model', None)
+        self.exploration_noise_fraction = float(cfg_utils.cfg_get(data, 'exploration_noise_fraction', 0.1))
+
+        # RedQ specific parameters
+        self.num_critics = int(cfg_utils.cfg_get(data, 'num_critics', 10))
+        self.utd_ratio = int(cfg_utils.cfg_get(data, 'utd_ratio', 20))
+        self.num_min = int(cfg_utils.cfg_get(data, 'num_min', 2))
+        redq_log.info(f'Number of critics: {self.num_critics}')
+        redq_log.info(f'UTD ratio: {self.utd_ratio}')
+        redq_log.info(f'Num min (M): {self.num_min}')
+
+        self.actor_model_type = cfg_utils.cfg_get(data, 'actor_model', "actor_fcnn-v0")
+        self.critic_model_type = cfg_utils.cfg_get(data, 'critic_model', "critic_fcnn-v0")
+
+        self.logdir = logdir
+
+        self.mse_loss = tf.keras.losses.MeanSquaredError()
+
+        # Buffer
+        if buffer_type is None:
+            self.buffer_type = cfg_utils.cfg_get(data, 'buffer_type', None)
+        else:
+            self.buffer_type = buffer_type
+
+        self.buffer = jlab_opt_control.buffers.make(
+            self.buffer_type, state_dim=self.num_states, action_dim=self.num_actions, logdir=self.logdir, buffer_size=buffer_size)
+        self.buffer.save_cfg()
+
+        # Used to update target networks
+        self.tau = float(cfg_utils.cfg_get(data, 'tau', 0.005))
+        self.gamma = float(cfg_utils.cfg_get(data, 'discount', 0.99))
+
+        # Setup Optimizers
+        self.critic_lr = float(cfg_utils.cfg_get(
+            data, 'critic_learning_rate', 5e-4))
+        self.actor_lr = float(cfg_utils.cfg_get(
+            data, 'actor_learning_rate', 1e-4))
+
+        # Always use legacy optimizers to avoid variable tracking issues
+        redq_log.info('Using legacy Adam optimizers')
+        self.critic_optimizer = tf.keras.optimizers.legacy.Adam(
+            self.critic_lr, epsilon=1e-08)
+        self.actor_optimizer = tf.keras.optimizers.legacy.Adam(
+            self.actor_lr, epsilon=1e-08)
+
+        self.initialize_new_models()
+
+        # Load models for retraining
+        if self.model_load_path is not None:
+            self.load()
+
+        # update counting
+        self.ntrain_calls = 0
+        self.train_steps = 0
+        self.actor_update_freq = int(
+            cfg_utils.cfg_get(data, 'actor_update_freq', 2))
+        self.critic_update_freq = int(
+            cfg_utils.cfg_get(data, 'critic_update_freq', 2))
+
+        self.noise_clip = 0.5
+
+        try:
+            os.mkdir(self.logdir)
+        except OSError as error:
+            redq_log.warning(error)
+        file_writer = tf.summary.create_file_writer(self.logdir + '/metrics')
+        file_writer.set_as_default()
+        self.nactions = 0
+
+    def initialize_new_models(self):
+        """ Initialize new models from scratch """
+        redq_log.info('Running KerasREDQTD3 initialize_new_models()')
+
+        self.actor_model = jlab_opt_control.models.make(
+            self.actor_model_type, state_dim=self.num_states, action_dim=self.num_actions, min_action=self.lower_bound, max_action=self.upper_bound, logdir=self.logdir)
+        self.target_actor = jlab_opt_control.models.make(
+            self.actor_model_type, state_dim=self.num_states, action_dim=self.num_actions, min_action=self.lower_bound, max_action=self.upper_bound, logdir=self.logdir)
+
+        # Run through model once to initialize variables
+        self.actor_model(tf.zeros([1, self.num_states]))
+        self.target_actor(tf.zeros([1, self.num_states]))
+
+        self.actor_model.save_cfg()
+
+        # Initialize ensemble of critics
+        for i in range(self.num_critics):
+            # Use time for different seeds
+            seed = time.time_ns()
+            str_seed = str(seed)
+            seed = int(str_seed[9:-3])
+            redq_log.debug(f'seed for critic {i}: {seed}')
+            tf.random.set_seed(seed)
+            
+            critic_model = jlab_opt_control.models.make(
+                self.critic_model_type, state_dim=self.num_states, action_dim=self.num_actions, logdir=self.logdir)
+            target_critic = jlab_opt_control.models.make(
+                self.critic_model_type, state_dim=self.num_states, action_dim=self.num_actions, logdir=self.logdir)
+            
+            # Run through model once to initialize variables
+            critic_model(tf.zeros([1, self.num_states]), tf.zeros([1, self.num_actions]))
+            target_critic(tf.zeros([1, self.num_states]), tf.zeros([1, self.num_actions]))
+            
+            # Copy weights from critic model to target critic
+            target_critic.set_weights(critic_model.get_weights())
+            
+            self.critic_models.append(critic_model)
+            self.target_critics.append(target_critic)
+            
+            # Only save config for the first critic (they all have the same architecture)
+            if i == 0:
+                critic_model.save_cfg()
+            
+            # Sleep a bit to make sure each critic gets a different seed
+            time.sleep(1 / 10)
+        
+        # Copy weights from actor model to target actor
+        self.target_actor.set_weights(self.actor_model.get_weights())
+
+    def train_critics(self, states, actions, rewards, next_states, dones, weights, critic_indices):
+        # Generate the proper noise
+        noise = (tf.random.normal(tf.shape(actions), dtype=tf.float32) * 0.2)
+        noise_clipped = tf.clip_by_value(
+            noise, -self.noise_clip, self.noise_clip) * self.target_actor.action_scale
+        next_actions = tf.clip_by_value(self.target_actor(
+            next_states, training=False) + noise_clipped, self.lower_bound, self.upper_bound)
+
+        # Get Q-values from all target critics
+        target_q_values = []
+        for idx in critic_indices:
+            target_q = self.target_critics[idx](next_states, next_actions, training=False)
+            target_q_values.append(target_q)
+        
+        # Find the minimum Q-value among the selected critics (in-target minimization)
+        target_q_stack = tf.stack(target_q_values, axis=1)
+        target_q_min = tf.reduce_min(target_q_stack, axis=1)
+
+        # Bellman equation for the q value
+        q_targets = rewards + self.gamma * target_q_min * (1.0 - dones)
+
+        critic_losses = []
+        td_errors_list = []
+        
+        # Update each critic separately to avoid variable tracking issues
+        for i, critic_model in enumerate(self.critic_models):
+            with tf.GradientTape() as tape:
+                q_values = critic_model(states, actions, training=True)
+                td_errors = q_values - q_targets
+                critic_loss = self.mse_loss(q_values, q_targets, sample_weight=weights)
+                
+            gradients = tape.gradient(critic_loss, critic_model.trainable_variables)
+            self.critic_optimizer.apply_gradients(zip(gradients, critic_model.trainable_variables))
+            
+            critic_losses.append(critic_loss)
+            td_errors_list.append(tf.abs(td_errors))
+        
+        # Average TD errors for priority updates
+        td_errors_avg = tf.reduce_mean(tf.stack(td_errors_list), axis=0)
+        
+        return critic_losses, td_errors_avg
+
+    def train_actor(self, states):
+        # Use random critic from ensemble for policy update
+        random_critic_idx = np.random.randint(0, self.num_critics)
+        
+        with tf.GradientTape() as tape:
+            actions = self.actor_model(states, training=True)
+            q_value = self.critic_models[random_critic_idx](states, actions, training=False)
+            loss = -tf.math.reduce_mean(q_value)
+
+        gradient = tape.gradient(loss, self.actor_model.trainable_variables)
+        self.actor_optimizer.apply_gradients(
+            zip(gradient, self.actor_model.trainable_variables))
+        return loss
+
+    @tf.function
+    def soft_update(self, target_weights, weights):
+        for (target_weight, weight) in zip(target_weights, weights):
+            target_weight.assign(weight * self.tau +
+                                 target_weight * (1.0 - self.tau))
+
+    def train(self):
+        """ Method used to train with higher UTD ratio """
+        self.ntrain_calls += 1
+
+        if self.buffer.size() > np.max([self.batch_size, self.warmup_size]):
+            # Perform multiple updates per environment step (higher UTD ratio)
+            for _ in range(self.utd_ratio):
+                self.train_steps += 1
+                
+                # Get samples
+                states, actions, rewards, next_states, dones, weights = self.buffer.sample(
+                    self.batch_size)
+
+                # Convert to tensors
+                state_batch = tf.convert_to_tensor(states, dtype=tf.float32)
+                action_batch = tf.convert_to_tensor(actions, dtype=tf.float32)
+                reward_batch = tf.convert_to_tensor(rewards, dtype=tf.float32)
+                next_state_batch = tf.convert_to_tensor(next_states, dtype=tf.float32)
+                done_batch = tf.convert_to_tensor(dones, dtype=tf.float32)
+                weights_batch = tf.convert_to_tensor(weights, dtype=tf.float32)
+                
+                # Randomly select subset of critics for in-target minimization
+                critic_indices = random.sample(range(self.num_critics), self.num_min)
+                
+                # Train critics
+                critic_losses, td_errors = self.train_critics(
+                    state_batch, action_batch, reward_batch,
+                    next_state_batch, done_batch, weights_batch,
+                    critic_indices)
+
+                # Log losses for the first two critics (for visualization)
+                if len(critic_losses) >= 2:
+                    tf.summary.scalar('Critic Loss 1', data=critic_losses[0],
+                                      step=int(self.train_steps))
+                    tf.summary.scalar('Critic Loss 2', data=critic_losses[1],
+                                      step=int(self.train_steps))
+
+                # Update Priorities if using PER
+                if "PER" in self.buffer_type:
+                    new_priorities = td_errors.numpy()
+                    self.buffer.update_priorities(new_priorities)
+
+                # Update actor and target networks
+                if self.train_steps % self.actor_update_freq == 0:
+                    actor_loss = self.train_actor(state_batch)
+                    tf.summary.scalar('Actor Loss', data=actor_loss,
+                                      step=int(self.train_steps))
+                    self.soft_update(self.target_actor.variables,
+                                     self.actor_model.variables)
+
+                if self.train_steps % self.critic_update_freq == 0:
+                    for i in range(self.num_critics):
+                        self.soft_update(self.target_critics[i].variables,
+                                         self.critic_models[i].variables)
+
+    def action(self, state, train=True, inference=False):
+        """ Method used to provide the next action using the target model """
+        # Warmup experience sample
+        if (self.buffer.size() < np.max([self.batch_size, self.warmup_size])) and inference == False:
+            sampled_action = self.env.action_space.sample()
+            noise = np.zeros(self.num_actions)
+        # Warmup completed, sample from actor or run inference
+        else:
+            state = tf.expand_dims(state, 0)
+            sampled_action = (self.actor_model(state)).numpy()
+            if train:
+                noise = (tf.random.normal(shape=(self.num_actions,), mean=0,
+                         stddev=self.actor_model.action_scale * self.exploration_noise_fraction, dtype=tf.float32)).numpy()
+                sampled_action = np.clip(
+                    sampled_action + noise, self.lower_bound, self.upper_bound)
+            else:
+                noise = np.zeros(self.num_actions)
+
+            sampled_action = sampled_action.flatten()
+            noise = noise.flatten()
+            assert sampled_action.shape == self.num_actions or sampled_action.shape == (self.num_actions,), \
+                f"Sampled action shape is incorrect... {sampled_action.shape}"
+
+        # Log the training action(s) taken
+        if train:
+            self.nactions = self.nactions + 1
+            if self.num_actions == 0:
+                tf.summary.scalar('Action', data=sampled_action,
+                                  step=int(self.nactions))
+            else:
+                for i in range(self.num_actions):
+                    tf.summary.scalar('Action #{}'.format(
+                        i), data=sampled_action[i], step=int(self.nactions))
+
+        # Insure action output by actor is in legal environment range
+        return sampled_action, noise
+
+    def memory(self, obs_tuple):
+        memory_with_default_priority = obs_tuple + (self.buffer.max_priority,)
+        self.buffer.record(memory_with_default_priority)
+
+    def load(self):
+        """ Load the ML models """
+        try:
+            model_load_count = 0
+            expected_models = 1 + 1 + self.num_critics * 2  # actor + target_actor + num_critics*(critic+target)
+            
+            # Load actor models
+            for file in os.listdir(self.model_load_path):
+                if 'actor_model' in file and file.endswith('.h5'):
+                    self.actor_model.load_weights(join(self.model_load_path, file))
+                    model_load_count += 1
+                elif 'target_actor' in file and file.endswith('.h5'):
+                    self.target_actor.load_weights(join(self.model_load_path, file))
+                    model_load_count += 1
+            
+            # Load critic models - pattern for filenames must match save method
+            for i in range(self.num_critics):
+                for file in os.listdir(self.model_load_path):
+                    if f'critic_model_{i}' in file and file.endswith('.h5'):
+                        self.critic_models[i].load_weights(join(self.model_load_path, file))
+                        model_load_count += 1
+                    elif f'target_critic_{i}' in file and file.endswith('.h5'):
+                        self.target_critics[i].load_weights(join(self.model_load_path, file))
+                        model_load_count += 1
+            
+            if model_load_count == expected_models:
+                redq_log.info('Models loaded successfully')
+            else:
+                redq_log.error(f'Models not loaded properly, loaded {model_load_count}/{expected_models}')
+        except Exception as e:
+            redq_log.error(f"Error while loading models: {e}, initializing new models...")
+
+    def save(self, post_fix="test"):
+        """ Save the ML models """
+        try:
+            destination_file_path = os.path.join(self.logdir, 'models/')
+            if not os.path.exists(destination_file_path):
+                os.makedirs(destination_file_path)
+
+            destination_file_path = os.path.join(destination_file_path, post_fix + '/')
+            if not os.path.exists(destination_file_path):
+                os.makedirs(destination_file_path)
+
+            # Save actor models
+            self.actor_model.save_weights(
+                join(destination_file_path, f"actor_model_{post_fix}.h5"))
+            self.target_actor.save_weights(
+                join(destination_file_path, f"target_actor_{post_fix}.h5"))
+            
+            # Save all critic models in ensemble
+            for i, (critic_model, target_critic) in enumerate(zip(self.critic_models, self.target_critics)):
+                critic_model.save_weights(
+                    join(destination_file_path, f"critic_model_{i}_{post_fix}.h5"))
+                target_critic.save_weights(
+                    join(destination_file_path, f"target_critic_{i}_{post_fix}.h5"))
+                
+            redq_log.info('Agent models saved successfully')
+        except Exception as e:
+            redq_log.error(f"Error in saving the models: {e}")
+
+    def save_cfg(self):
+        """ Save the actor cfg """
+        try:
+            destination_file_path = os.path.join(self.logdir, 'cfgs/')
+            if not os.path.exists(destination_file_path):
+                os.makedirs(destination_file_path)
+            destination_file_path = os.path.join(
+                destination_file_path, os.path.basename(self.pfn_json_file))
+            shutil.copy(self.pfn_json_file, destination_file_path)
+            redq_log.info('Agent config saved successfully')
+        except:
+            redq_log.error("Error in saving the agent cfg...")
