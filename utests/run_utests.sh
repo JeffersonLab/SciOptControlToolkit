f3df97a5377c0151e331983b25d0b2a805e23522
schram <schram@jlab.org>
2025-03-31 16:54:50 -0400

    Update script to work.

--- utests/run_utests.sh
+++ utests/run_utests.sh
@@ -2,9 +2,9 @@
 
 REPO_PATH=$(git rev-parse --show-toplevel)/utests
 echo 'Repo directory '$REPO_PATH
-PATTERN="*.py"
 
-for filename in "$REPO_PATH"/$PATTERN; do
+#for filename in $REPO_PATH/$PATTERN; do
+for filename in ${REPO_PATH}/*.py; do
   echo 'Running utest: '$filename
   python $filename
 done
