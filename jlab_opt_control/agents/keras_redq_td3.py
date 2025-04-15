b34152558dd26b19b4656915129860d94942a370
armenkasp <armenk@jlab.org>
2025-04-15 11:03:53 -0400

    Utilized inheritence to reduce code reuse from TD3

--- jlab_opt_control/agents/keras_redq_td3.py
+++ jlab_opt_control/agents/keras_redq_td3.py
@@ -1,145 +1,106 @@
+# Copyright (c) 2020, Jefferson Science Associates, LLC. All Rights Reserved. Redistribution
+# and use in source and binary forms, with or without modification, are permitted as a
+# licensed user provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this
+#    list of conditions and the following disclaimer in the documentation and/or other
+#    materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# This material resulted from work developed under a United States Government Contract.
+# The Government retains a paid-up, nonexclusive, irrevocable worldwide license in such
+# copyrighted data to reproduce, distribute copies to the public, prepare derivative works,
+# perform publicly and display publicly and to permit others to do so.
+#
+# THIS SOFTWARE IS PROVIDED BY JEFFERSON SCIENCE ASSOCIATES LLC "AS IS" AND ANY EXPRESS
+# OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+# JEFFERSON SCIENCE ASSOCIATES, LLC OR THE U.S. GOVERNMENT BE LIABLE TO LICENSEE OR ANY
+# THIRD PARTES FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+# OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 import logging
 import jlab_opt_control as jlab_opt_control
 import jlab_opt_control.utils.cfg_utils as cfg_utils
-import jlab_opt_control.buffers
-import jlab_opt_control.models
 import tensorflow as tf
-from tensorflow.keras import layers
 import numpy as np
 import os
 from os.path import join
 import time
 import json
+import random
 import platform
 import sys
 import shutil
-import random
-processor = platform.processor()
+
+from jlab_opt_control.agents.keras_td3 import KerasTD3
 
 redq_log = logging.getLogger("REDQ-TD3-Agent")
 redq_log.setLevel(logging.DEBUG)
-logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
 
 
-class KerasREDQTD3(jlab_opt_control.Agent):
+class KerasREDQTD3(KerasTD3):
+    """
+    REDQ-TD3 implementation inheriting from TD3.
+    Uses an ensemble of critics and higher update-to-data ratio.
+    """
 
     def __init__(self, env, logdir, buffer_type=None, buffer_size=None, cfg='keras_redq_td3.json'):
-        """ Define all key variables required for all agent """
-
-        # Get env info
-        self.critic_models = []
-        self.target_critics = []
-        self.target_actor = None
-        self.actor_model = None
-        redq_log.info('Running KerasREDQTD3 __init__')
-
-        # Environment setup
-        self.env = env
-        try:
-            assert "Box" in str(type(env.action_space)), 'Invalid action space'
-            self.num_states = env.observation_space.shape[0]
-            self.num_actions = env.action_space.shape[0]
-            self.upper_bound = env.action_space.high
-            self.lower_bound = env.action_space.low
-            redq_log.info(f'Action upper bound: {self.upper_bound}')
-            redq_log.info(
-                f'Action upper bound: {float(env.action_space.high[0])}')
-            redq_log.info(f'Action lower bound: {self.lower_bound}')
-            redq_log.info(
-                f'Action lower bound: {float(env.action_space.low[0])}')
-            self.range = self.upper_bound - self.lower_bound
-            redq_log.info(f'Action range: {self.range}')
-        except:
-            redq_log.error('Action space not valid for this agent.')
-            sys.exit(0)
-
-        # Load configuration
+        """
+        Initialize REDQ-TD3 agent by reusing TD3 initialization and adding REDQ-specific components.
+        """
+        # First, load the configuration to get REDQ-specific parameters
         absolute_path = os.path.dirname(__file__)
         relative_path = "../cfgs/"
         full_path = os.path.join(absolute_path, relative_path)
-        self.pfn_json_file = os.path.join(full_path, cfg)
-        redq_log.debug(f'pfn_json_file:{self.pfn_json_file}')
-        with open(self.pfn_json_file) as json_file:
+        pfn_json_file = os.path.join(full_path, cfg)
+        
+        with open(pfn_json_file) as json_file:
             data = json.load(json_file)
-        self.warmup_size = int(cfg_utils.cfg_get(data, 'warmup_size', 10000))
-        self.batch_size = int(cfg_utils.cfg_get(data, 'batch_size', 100))
-        self.model_load_path = cfg_utils.cfg_get(data, 'load_model', None)
-        self.exploration_noise_fraction = float(cfg_utils.cfg_get(data, 'exploration_noise_fraction', 0.1))
-
-        # RedQ specific parameters
+            
+        # Set REDQ-specific attributes before parent initialization
+        self.critic_models = []
+        self.target_critics = []
         self.num_critics = int(cfg_utils.cfg_get(data, 'num_critics', 10))
         self.utd_ratio = int(cfg_utils.cfg_get(data, 'utd_ratio', 20))
         self.num_min = int(cfg_utils.cfg_get(data, 'num_min', 2))
-        redq_log.info(f'Number of critics: {self.num_critics}')
-        redq_log.info(f'UTD ratio: {self.utd_ratio}')
-        redq_log.info(f'Num min (M): {self.num_min}')
-
-        self.actor_model_type = cfg_utils.cfg_get(data, 'actor_model', "actor_fcnn-v0")
-        self.critic_model_type = cfg_utils.cfg_get(data, 'critic_model', "critic_fcnn-v0")
-
-        self.logdir = logdir
-
-        self.mse_loss = tf.keras.losses.MeanSquaredError()
-
-        # Buffer
-        if buffer_type is None:
-            self.buffer_type = cfg_utils.cfg_get(data, 'buffer_type', None)
-        else:
-            self.buffer_type = buffer_type
-
-        self.buffer = jlab_opt_control.buffers.make(
-            self.buffer_type, state_dim=self.num_states, action_dim=self.num_actions, logdir=self.logdir, buffer_size=buffer_size)
-        self.buffer.save_cfg()
-
-        # Used to update target networks
-        self.tau = float(cfg_utils.cfg_get(data, 'tau', 0.005))
-        self.gamma = float(cfg_utils.cfg_get(data, 'discount', 0.99))
-
-        # Setup Optimizers
-        self.critic_lr = float(cfg_utils.cfg_get(
-            data, 'critic_learning_rate', 5e-4))
-        self.actor_lr = float(cfg_utils.cfg_get(
-            data, 'actor_learning_rate', 1e-4))
-
-        # Always use legacy optimizers to avoid variable tracking issues
+        self.train_steps = 0
+        
+        # Call parent initialization with modified parameters
+        super().__init__(env, logdir, buffer_type, buffer_size, cfg)
+        
+        # Override parent's optimizers with legacy versions
         redq_log.info('Using legacy Adam optimizers')
         self.critic_optimizer = tf.keras.optimizers.legacy.Adam(
             self.critic_lr, epsilon=1e-08)
         self.actor_optimizer = tf.keras.optimizers.legacy.Adam(
             self.actor_lr, epsilon=1e-08)
-
-        self.initialize_new_models()
-
-        # Load models for retraining
-        if self.model_load_path is not None:
-            self.load()
-
-        # update counting
-        self.ntrain_calls = 0
-        self.train_steps = 0
-        self.actor_update_freq = int(
-            cfg_utils.cfg_get(data, 'actor_update_freq', 2))
-        self.critic_update_freq = int(
-            cfg_utils.cfg_get(data, 'critic_update_freq', 2))
-
-        self.noise_clip = 0.5
-
-        try:
-            os.mkdir(self.logdir)
-        except OSError as error:
-            redq_log.warning(error)
-        file_writer = tf.summary.create_file_writer(self.logdir + '/metrics')
-        file_writer.set_as_default()
-        self.nactions = 0
+        
+        redq_log.info('Running KerasREDQTD3 __init__')
+        redq_log.info(f'Number of critics: {self.num_critics}')
+        redq_log.info(f'UTD ratio: {self.utd_ratio}')
+        redq_log.info(f'Num min (M): {self.num_min}')
 
     def initialize_new_models(self):
-        """ Initialize new models from scratch """
+        """
+        Override parent's model initialization to create an ensemble of critics.
+        """
         redq_log.info('Running KerasREDQTD3 initialize_new_models()')
 
+        # Initialize actor and target actor (same as TD3)
         self.actor_model = jlab_opt_control.models.make(
-            self.actor_model_type, state_dim=self.num_states, action_dim=self.num_actions, min_action=self.lower_bound, max_action=self.upper_bound, logdir=self.logdir)
+            self.actor_model_type, state_dim=self.num_states, action_dim=self.num_actions, 
+            min_action=self.lower_bound, max_action=self.upper_bound, logdir=self.logdir)
         self.target_actor = jlab_opt_control.models.make(
-            self.actor_model_type, state_dim=self.num_states, action_dim=self.num_actions, min_action=self.lower_bound, max_action=self.upper_bound, logdir=self.logdir)
+            self.actor_model_type, state_dim=self.num_states, action_dim=self.num_actions, 
+            min_action=self.lower_bound, max_action=self.upper_bound, logdir=self.logdir)
 
         # Run through model once to initialize variables
         self.actor_model(tf.zeros([1, self.num_states]))
@@ -147,9 +108,8 @@ class KerasREDQTD3(jlab_opt_control.Agent):
 
         self.actor_model.save_cfg()
 
-        # Initialize ensemble of critics
+        # Initialize ensemble of critics (REDQ-specific)
         for i in range(self.num_critics):
-            # Use time for different seeds
             seed = time.time_ns()
             str_seed = str(seed)
             seed = int(str_seed[9:-3])
@@ -175,13 +135,16 @@ class KerasREDQTD3(jlab_opt_control.Agent):
             if i == 0:
                 critic_model.save_cfg()
             
-            # Sleep a bit to make sure each critic gets a different seed
+            # Sleep for a different seed
             time.sleep(1 / 10)
         
         # Copy weights from actor model to target actor
         self.target_actor.set_weights(self.actor_model.get_weights())
 
     def train_critics(self, states, actions, rewards, next_states, dones, weights, critic_indices):
+        """
+        Train ensemble of critics with in-target minimization of subset of critics.
+        """
         # Generate the proper noise
         noise = (tf.random.normal(tf.shape(actions), dtype=tf.float32) * 0.2)
         noise_clipped = tf.clip_by_value(
@@ -189,7 +152,7 @@ class KerasREDQTD3(jlab_opt_control.Agent):
         next_actions = tf.clip_by_value(self.target_actor(
             next_states, training=False) + noise_clipped, self.lower_bound, self.upper_bound)
 
-        # Get Q-values from all target critics
+        # Get Q-values from selected target critics
         target_q_values = []
         for idx in critic_indices:
             target_q = self.target_critics[idx](next_states, next_actions, training=False)
@@ -205,7 +168,7 @@ class KerasREDQTD3(jlab_opt_control.Agent):
         critic_losses = []
         td_errors_list = []
         
-        # Update each critic separately to avoid variable tracking issues
+        # Update each critic separately using the legacy optimizer
         for i, critic_model in enumerate(self.critic_models):
             with tf.GradientTape() as tape:
                 q_values = critic_model(states, actions, training=True)
@@ -224,6 +187,9 @@ class KerasREDQTD3(jlab_opt_control.Agent):
         return critic_losses, td_errors_avg
 
     def train_actor(self, states):
+        """
+        Override parent's actor training to use random critic from ensemble.
+        """
         # Use random critic from ensemble for policy update
         random_critic_idx = np.random.randint(0, self.num_critics)
         
@@ -237,14 +203,10 @@ class KerasREDQTD3(jlab_opt_control.Agent):
             zip(gradient, self.actor_model.trainable_variables))
         return loss
 
-    @tf.function
-    def soft_update(self, target_weights, weights):
-        for (target_weight, weight) in zip(target_weights, weights):
-            target_weight.assign(weight * self.tau +
-                                 target_weight * (1.0 - self.tau))
-
     def train(self):
-        """ Method used to train with higher UTD ratio """
+        """
+        Override parent's train method to implement higher UTD ratio.
+        """
         self.ntrain_calls += 1
 
         if self.buffer.size() > np.max([self.batch_size, self.warmup_size]):
@@ -276,9 +238,9 @@ class KerasREDQTD3(jlab_opt_control.Agent):
                 # Log losses for the first two critics (for visualization)
                 if len(critic_losses) >= 2:
                     tf.summary.scalar('Critic Loss 1', data=critic_losses[0],
-                                      step=int(self.train_steps))
+                                    step=int(self.train_steps))
                     tf.summary.scalar('Critic Loss 2', data=critic_losses[1],
-                                      step=int(self.train_steps))
+                                    step=int(self.train_steps))
 
                 # Update Priorities if using PER
                 if "PER" in self.buffer_type:
@@ -289,58 +251,19 @@ class KerasREDQTD3(jlab_opt_control.Agent):
                 if self.train_steps % self.actor_update_freq == 0:
                     actor_loss = self.train_actor(state_batch)
                     tf.summary.scalar('Actor Loss', data=actor_loss,
-                                      step=int(self.train_steps))
+                                    step=int(self.train_steps))
                     self.soft_update(self.target_actor.variables,
-                                     self.actor_model.variables)
+                                    self.actor_model.variables)
 
                 if self.train_steps % self.critic_update_freq == 0:
                     for i in range(self.num_critics):
                         self.soft_update(self.target_critics[i].variables,
-                                         self.critic_models[i].variables)
-
-    def action(self, state, train=True, inference=False):
-        """ Method used to provide the next action using the target model """
-        # Warmup experience sample
-        if (self.buffer.size() < np.max([self.batch_size, self.warmup_size])) and inference == False:
-            sampled_action = self.env.action_space.sample()
-            noise = np.zeros(self.num_actions)
-        # Warmup completed, sample from actor or run inference
-        else:
-            state = tf.expand_dims(state, 0)
-            sampled_action = (self.actor_model(state)).numpy()
-            if train:
-                noise = (tf.random.normal(shape=(self.num_actions,), mean=0,
-                         stddev=self.actor_model.action_scale * self.exploration_noise_fraction, dtype=tf.float32)).numpy()
-                sampled_action = np.clip(
-                    sampled_action + noise, self.lower_bound, self.upper_bound)
-            else:
-                noise = np.zeros(self.num_actions)
-
-            sampled_action = sampled_action.flatten()
-            noise = noise.flatten()
-            assert sampled_action.shape == self.num_actions or sampled_action.shape == (self.num_actions,), \
-                f"Sampled action shape is incorrect... {sampled_action.shape}"
-
-        # Log the training action(s) taken
-        if train:
-            self.nactions = self.nactions + 1
-            if self.num_actions == 0:
-                tf.summary.scalar('Action', data=sampled_action,
-                                  step=int(self.nactions))
-            else:
-                for i in range(self.num_actions):
-                    tf.summary.scalar('Action #{}'.format(
-                        i), data=sampled_action[i], step=int(self.nactions))
-
-        # Insure action output by actor is in legal environment range
-        return sampled_action, noise
-
-    def memory(self, obs_tuple):
-        memory_with_default_priority = obs_tuple + (self.buffer.max_priority,)
-        self.buffer.record(memory_with_default_priority)
+                                        self.critic_models[i].variables)
 
     def load(self):
-        """ Load the ML models """
+        """
+        Override parent's load method to handle ensemble of critics.
+        """
         try:
             model_load_count = 0
             expected_models = 1 + 1 + self.num_critics * 2  # actor + target_actor + num_critics*(critic+target)
@@ -371,8 +294,10 @@ class KerasREDQTD3(jlab_opt_control.Agent):
         except Exception as e:
             redq_log.error(f"Error while loading models: {e}, initializing new models...")
 
-    def save(self, post_fix="test"):
-        """ Save the ML models """
+    def save(self, post_fix="default"):
+        """
+        Override parent's save method to handle ensemble of critics.
+        """
         try:
             destination_file_path = os.path.join(self.logdir, 'models/')
             if not os.path.exists(destination_file_path):
@@ -398,16 +323,3 @@ class KerasREDQTD3(jlab_opt_control.Agent):
             redq_log.info('Agent models saved successfully')
         except Exception as e:
             redq_log.error(f"Error in saving the models: {e}")
-
-    def save_cfg(self):
-        """ Save the actor cfg """
-        try:
-            destination_file_path = os.path.join(self.logdir, 'cfgs/')
-            if not os.path.exists(destination_file_path):
-                os.makedirs(destination_file_path)
-            destination_file_path = os.path.join(
-                destination_file_path, os.path.basename(self.pfn_json_file))
-            shutil.copy(self.pfn_json_file, destination_file_path)
-            redq_log.info('Agent config saved successfully')
-        except:
-            redq_log.error("Error in saving the agent cfg...")
