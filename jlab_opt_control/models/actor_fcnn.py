bae99b09190fb932babb652d31714625f67b1ebb
Kishanrajput <kishansingh0817@gmail.com>
2025-10-30 12:03:08 -0400

    add object variables for inheritance

--- jlab_opt_control/models/actor_fcnn.py
+++ jlab_opt_control/models/actor_fcnn.py
@@ -27,29 +27,29 @@ class ActorFCNN(Model):
         with open(self.pfn_json_file, 'r') as f:
             cfg_data = json.load(f)
         hidden_layers = cfg_data.get('hidden_layers', 2)  # Default to 2 if not specified
-        nodes_per_layer = cfg_data.get('nodes_per_layer', [256, 256])  # Default
-        activation_functions = cfg_data.get('activation_functions', ["relu"] * hidden_layers + ["tanh"])  # Defaults
+        self.nodes_per_layer = cfg_data.get('nodes_per_layer', [256, 256])  # Default
+        self.activation_functions = cfg_data.get('activation_functions', ["relu"] * hidden_layers + ["tanh"])  # Defaults
         kernel_initializer = cfg_data.get('kernel_initializer', 'glorot_uniform')
  
         self.logdir = logdir
 
         # Error Checking
-        if hidden_layers != len(nodes_per_layer) or hidden_layers != len(activation_functions)-1:
-            if hidden_layers != len(nodes_per_layer):
+        if hidden_layers != len(self.nodes_per_layer) or hidden_layers != len(self.activation_functions)-1:
+            if hidden_layers != len(self.nodes_per_layer):
                 act_log.error("Number of nodes per layer does not match the number of hidden layers in the config.")
             else:  # hidden_layers != len(activation_functions)+1
                 act_log.error("Number of activation functions (+1 for output layer) does not match the number of hidden layers in the config.")
-        if activation_functions[-1] not in ["tanh"]:
+        if self.activation_functions[-1] not in ["tanh"]:
             act_log.error("Final layer activation function needs to be tanh. Scaling for the action space will not work as intended.")
 
         # Dynamic Actor Architecture
         self.hidden_layers = []
         for i in range(hidden_layers):
             # Layer construction with dynamic activation functions
-            self.hidden_layers.append(layers.Dense(nodes_per_layer[i], use_bias=False, kernel_initializer=kernel_initializer, input_shape=(state_dim,) if i == 0 else ()))
-            self.hidden_layers.append(layers.Activation(activation_functions[i]))
+            self.hidden_layers.append(layers.Dense(self.nodes_per_layer[i], use_bias=False, kernel_initializer=kernel_initializer, input_shape=(state_dim,) if i == 0 else ()))
+            self.hidden_layers.append(layers.Activation(self.activation_functions[i]))
         # Output layer with its specified activation function
-        self.output_layer = layers.Dense(action_dim, activation=activation_functions[-1])
+        self.output_layer = layers.Dense(action_dim, activation=self.activation_functions[-1])
 
         self.action_scale = tf.constant((max_action - min_action) / 2, dtype=tf.float32)
         self.action_bias = tf.constant((max_action + min_action) / 2, dtype=tf.float32)
