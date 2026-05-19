c760836d79daf69d1306b8a4a57431913dbc141b
armenk-admin <armenk-admin@cstarmenk-mbp.jlab.org>
2026-05-19 10:44:21 -0400

    Cannot spell python for the life of me

--- utests/run_branch_utests.sh
+++ utests/run_branch_utests.sh
@@ -3,6 +3,6 @@ set -e
 
 python utests/utest_agents.py
 python utests/utest_buffers.py
-pyhton utests/utest_envs_and_utils.py
-pyhton utests/utest_models.py
+python utests/utest_envs_and_utils.py
+python utests/utest_models.py
 python utests/utest_registry.py
